73ee0f163874616cfed773a09d56613f630db39c
Parbat Limbu <50946258+Habray@users.noreply.github.com>
2019-12-29 19:10:09 +0545

    Short program to convert gray image to negative

--- gray_to_negative_short.py
+++ gray_to_negative_short.py
@@ -1,7 +1,6 @@
 import matplotlib.pyplot as plt
 
-path = '/home/habray/Image_Processing/Working_Folder/Image-Processing/image/'
-
+path = '/home/habray/Image_Processing/Image-Processing/image/'
 img = plt.imread(path + 'panda.jpg')
 
 plt.imshow(255 - img, cmap="gray")
